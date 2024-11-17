18fce95ac7c8e0ac9c3ca2b71cdfabab48fdbeb7
Pablo - Note +A <pquiroga@maisaedu.com.br>
2024-11-17 14:38:07 -0300

    update

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 function manipuladorDeErros(error, req, res, next) {
     if (error instanceof mongoose.Error.CastError) {
         res.status(400).json({ message: "Um ou mais dados fornecidos estão incorretos." });
+    } else if (error instanceof mongoose.Error.ValidationError) {
+        const mensagensErro = Object.values(error.errors)
+        .map(error => error.message)
+        .join("; ");
+
+        res.status(400).send({ message: `Os seguintes erros foram encontrados: ${mensagensErro}` });
     } else {
         res.status(500).json({ message: "Erro interno do servidor." });
     };
